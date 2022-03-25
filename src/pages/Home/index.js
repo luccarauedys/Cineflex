6a02edee46769ba89ad9c63dc852616141104d7b
luccarauedys <luccarauedys@outlook.com>
2022-03-25 13:27:02 -0300

    requisicao get e listagem de filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import { Header, Main, Movies, Movie } from './styles';
+import loading from '../../assets/img/loading.gif';
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const promise = axios
+      .get('https://mock-api.driven.com.br/api/v5/cineflex/movies')
+      .then((response) => setMovies(response.data));
+  }, []);
+
   return (
     <>
       <Header>
@@ -11,11 +22,19 @@ export default function Home() {
       <Main>
         <h2>Selecione o filme</h2>
         <Movies>
-          <Movie>
-            <Link to="">
-              <img src="" alt="" />
-            </Link>
-          </Movie>
+          {movies.length === 0 && (
+            <img src={loading} style={{ width: '100px' }} alt="Loading GIF" />
+          )}
+
+          {movies.map((movie) => {
+            return (
+              <Movie key={movie.id}>
+                <Link to={`/sessions/${movie.id}`}>
+                  <img src={movie.posterURL} alt={movie.title} />
+                </Link>
+              </Movie>
+            );
+          })}
         </Movies>
       </Main>
     </>
